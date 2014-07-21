666c5295711c4534e6f2fd7c9a69093f5b27938f
Patrick Steele-Idem <pnidem@gmail.com>
2014-07-21 17:51:55 -0600

    Initial commit

---
+++ test/raptor-cookies-test.js
@@ -0,0 +1,22 @@
+'use strict';
+var chai = require('chai');
+chai.Assertion.includeStack = true;
+require('chai').should();
+var expect = require('chai').expect;
+var nodePath = require('path');
+var fs = require('fs');
+var logger = require('raptor-logging').logger(module);
+
+
+describe('raptor-cookies' , function() {
+
+    beforeEach(function(done) {
+
+        done();
+    });
+
+    it('should ...', function() {
+        
+    });
+});
+
